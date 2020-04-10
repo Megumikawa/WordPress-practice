d94939edb56bf2c1a8f516d64a9ed97cc42597f8
MEGUMI <megumi19901016@gmail.com>
2020-04-10 15:36:00 +0900

    wordpressの引っ越し作業

---
+++ header.php
@@ -0,0 +1,116 @@
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Feuille</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/normalize.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+    <script src="<?php echo get_template_directory_uri() ?>/js/script.js"></script>
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <div class="header-wrapper">
+      <header class="header">
+        <nav id="js_btn_menu" class="btn-menu">
+          <img class="btn-menu-close" src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">
+          <img class="btn-menu-open" src="<?php echo get_template_directory_uri() ?>/images/hamburger.svg" alt="ハンバーガーメニュー" width="20" height="14">
+        </nav>
+        <h1 class="logo"><a href="index.html"><img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" alt="Feuille" width="72" height="38"></a></h1>
+        <nav class="pc-global-menu">
+          <ul>
+            <li><a href="#">NEW ITEM</a></li>
+            <li class="js-category-icon"><a href="category.html">CATEGORY<img src="<?php echo get_template_directory_uri() ?>/images/nav-downarrow.svg" alt="カテゴリーの詳細を表示" width="6" height="4"></a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">GUIDE</a></li>
+          </ul>
+          <!-- ▼カテゴリーメガメニュ▼ -->
+          <div class="js-category-icon">
+            <div class="megamenu">
+              <ul class="megamenu-content">
+                <li><a href="#">ピアス</a></li>
+                <li><a href="#">イヤリング</a></li>
+                <li><a href="#">その他</a></li>
+              </ul>
+              <div class="all-item"><a href="#">全てのアイテム</a></div>
+            </div>
+          </div>
+          <!-- ▲カテゴリーメガメニュ▲ -->
+        </nav>
+
+        <!-- ▼SP用nav▼ -->
+        <nav class="header-nav">
+          <ul>
+            <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/search-icon.svg" alt="検索する" width="21" height="20"></a></li>
+            <li class="sp-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/cart-icon.svg" alt="カートページへ" width="20" height="20"><span class="sp-cart-counter">0</span></a></li>
+          </ul>
+        </nav>
+        <div class="pc-header-nav">
+          <nav class="pc-nav-block">
+            <ul>
+              <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/login-icon.svg" alt="マイページへログインする" width="16" height="18">LOGIN</a></li>
+              <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/favorite-icon.svg" alt="お気に入り画面へアクセスする" width="20" height="18">FAVORITE</a></li>
+              <li class="pc-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/cart-icon.svg" alt="ショッピングカートへアクセスする" width="20" height="20"><span class="pc-cart-counter">0</span></a></li>
+            </ul>
+          </nav>
+          <form id="search_form">
+            <div class="pc-search-block">
+              <input id="searchbox" name="searchbox" type="search" placeholder="SEARCH ITEMS">
+              <input id="submit" type="submit" value="">
+            </div>
+          </form>
+        </div>
+      </header>
+    </div>
+    <!-- ▼ハンバーガーメニュー始まり▼ -->
+    <div id="js_menu_contents" class="hamburger-container">
+      <section class="hamburger-checked-items">
+        <h2 class="hamburger-title-block">CHECKED ITEMS</h2>
+        <p class="hamburger-sub-title">閲覧履歴</p>
+        <ul class="checked-items">
+          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-01.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-02.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-03.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-04.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-05.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+        </ul>
+      </section>
+      <div class="hamburger-main">
+        <ul class="hamburger-main-menu">
+          <li><a href="#">ログイン</a></li>
+          <li><a href="#">新規会員登録</a></li>
+          <li id="js_hamburger_search_category" class="hamburger-search-category">
+            <a>カテゴリーから探す</a>
+            <ul id="js_humburger_category_child" class="hamburger-category-child">
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="hamburger-footer">
+        <ul class="hamburger-footer-menu">
+          <li><a href="#">新着情報</a></li>
+          <li><a href="#">ショッピングガイド</a></li>
+          <li><a href="#">新着アイテム</a></li>
+          <li><a href="#">ショッピングカート</a></li>
+          <li><a href="#">プライバシーポリシー</a></li>
+          <li><a href="#">特定商取引に基づく表示</a>
+          <li><a href="#">会社概要</a></li>
+          <li><a href="#">よくあるご質問</a>
+          <li><a href="#">お問い合わせ</a></li>
+        </ul>
+        <ul class="hamburger-sns-block">
+          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-instagram.svg" alt="インスタグラムへアクセスする" width="57" height="57"></a></li>
+          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-twitter.svg" alt="ツイッターへアクセスする" width="71" height="57"></a></li>
+          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-facebook.svg" alt="フェイスブックへアクセスする" width="57" height="57"></a></li>
+        </ul>
+        <p class="js-btn-close"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">閉じる</p>
+      </div>
+    </div>
+    <!-- ▲ハンバーガーメニュー終わり▲ -->
---
+++ home.php
@@ -0,0 +1,165 @@
+<?php get_header();?>
+<main>
+  <div class="top-slider-wrapper">
+    <div class="top-slider">
+      <ul id="top_slider">
+        <li><img class="sp-top-slider" src="<?php echo get_template_directory_uri() ?>/images/sp/slide-img-01.jpg" alt="ピアス商品" width="375" height="375"><img class="pc-top-slider" src="<?php echo get_template_directory_uri() ?>/images/top-slide-img01.jpg" alt="ピアス商品" width="1000" height="460"></li>
+        <li><img class="sp-top-slider" src="<?php echo get_template_directory_uri() ?>/images/sp/slide-img-02.jpg" alt="Feuille Gallery-2000円以上購入で送料無料" width="375" height="377"><img class="pc-top-slider" src="<?php echo get_template_directory_uri() ?>/images/top-slide-img02.jpg" alt="Feuille Gallery-2000円以上購入で送料無料" width="1000" height="460"></li>
+        <li><img class="sp-top-slider" src="<?php echo get_template_directory_uri() ?>/images/sp/slide-img-03.jpg" alt="イヤリング商品の一覧へ" width="375" height="375"><img class="pc-top-slider" src="<?php echo get_template_directory_uri() ?>/images/top-slide-img03.jpg" alt="イヤリング商品の一覧へ" width="1000" height="460"></li>
+      </ul>
+    </div>
+  </div>
+  <dl class="news-block">
+    <dt><time datetime="2020-12-31">2018/12/31</time></dt>
+    <dd>sample text sample text sample text sample text sample text sample text sample text</dd>
+    <dt><time datetime="2020-12-31">2018/12/31</time></dt>
+    <dd>sample text sample text sample text sample text sample text sample text sample text</dd>
+  </dl>
+
+  <section class="section-new-item">
+    <h2 class="title-block">NEW ITEM</h2>
+    <p class="sub-title">新着アイテム</p>
+    <ul class="product-item">
+      <li>
+        <a href="detail.html"><img src="<?php echo get_template_directory_uri() ?>/images/new-item01.jpg" alt="スエードタッセルピアス" width="220" height="220"></a>
+        <h3 class="product-name">スエードタッセルとゴールドプレートピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item02.jpg" alt="スエードタッセルピアス" width="220" height="220">
+        <h3 class="product-name">スエードタッセルとゴールドプレートピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item03.jpg" alt="紐まるイヤリング" width="220" height="220">
+        <h3 class="product-name">紐まるイヤリング</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item04.jpg" alt="クリアチェーンピアス" width="220" height="220">
+        <h3 class="product-name">クリアチェーンピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item05.jpg" alt="ドライフラワーレジンピアス" width="220" height="220">
+        <h3 class="product-name">ドライフラワーレジンピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item06.jpg" alt="ドライフラワーレジンピアス" width="220" height="220">
+        <h3 class="product-name">ドライフラワーレジンピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item07.jpg" alt="ゴールドプレート x クリアフープの大ぶりピアス" width="220" height="220">
+        <h3 class="product-name">ゴールドプレート x クリアフープの大ぶりピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/new-item08.jpg" alt="ドライフラワーレジンピアス" width="220" height="220">
+        <h3 class="product-name">ドライフラワーレジンピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+    </ul>
+    <nav class="btn-view-all">
+      <a href="#">VIEW ALL<i class="fas fa-angle-right"></i></a>
+    </nav>
+  </section>
+
+  <section class="section-pickup-item">
+    <h2 class="title-block">PICKUP ITEM</h2>
+    <p class="sub-title">ピックアップアイテム</p>
+    <ul class="product-item">
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-01.jpg" alt="ターコイズピアス" width="165" height="165">
+        <h3 class="product-name">ターコイズとバーパーツピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-02.jpg" alt="2連フープとクリアビーズピアス" width="165" height="165">
+        <h3 class="product-name">2連フープとクリアビーズピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-03.jpg" alt="ターコイズとウェーブパーツピアス" width="165" height="165">
+        <h3 class="product-name">ターコイズとウェーブパーツピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-04.jpg" alt="ホワイトサンゴとウェーブパーツピアス" width="165" height="165">
+        <h3 class="product-name">ホワイトサンゴとウェーブパーツピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-05.jpg" alt="ターコイズとサークルパーツピアス" width="165" height="165">
+        <h3 class="product-name">ターコイズとサークルパーツピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri() ?>/images/pickup-item-06.jpg" alt="ミニかすみ草のドライフラワーのピアス" width="165" height="165">
+        <h3 class="product-name">ミニかすみ草のドライフラワーピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+    </ul>
+    <nav class="btn-view-all">
+      <a href="#">VIEW ALL<i class="fas fa-angle-right"></i></a>
+    </nav>
+  </section>
+
+  <section class="section-ranking">
+    <h2 class="title-block">RANKING</h2>
+    <p class="sub-title">ランキング</p>
+    <ol class="item-ranking">
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-01.jpg" alt="シェルとウェーブフープのピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">シェルとウェーブフープピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-02.jpg" alt="コットンパールと3連フープのピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">コットンパールと3連フープピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-03.jpg" alt="スエードタッセルとゴールドプレートピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">スエードタッセルとゴールドプレートピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-04.jpg" alt="ターコイズとウェーブパーツピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">ターコイズとウェーブパーツピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-05.jpg" alt="ドライフラワーレジンピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">ドライフラワーレジンピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+      <li>
+        <div class="icon-rank">
+          <img src="<?php echo get_template_directory_uri() ?>/images/ranking-item-06.jpg" alt="ミニかすみ草のドライフラワーピアス" width="165" height="165">
+        </div>
+        <h3 class="product-name">ミニかすみ草のドライフラワーピアス</h3>
+        <p class="product-price">¥00,000</p>
+      </li>
+    </ol>
+  </section>
+  <section class="section-banner-block">
+    <ul class="banner">
+      <li><img src="<?php echo get_template_directory_uri() ?>/images/bnr-img01.jpg" alt="ドライフラワーレジンピアス" width="240" height="80"></li>
+      <li><img src="<?php echo get_template_directory_uri() ?>/images/bnr-img02.jpg" alt="クリアチェーンピアス" width="240" height="80"></li>
+    </ul>
+  </section>
+</main>
+<div id="pagetop" class="pagetop"><img src="<?php echo get_template_directory_uri() ?>/images/pagetop.svg" alt="ページトップ" width="35" height="35"></div>
+<?php get_footer();?>
