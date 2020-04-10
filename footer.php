b00ee218e7fc854943e549a08c3bc748e57ab799
MEGUMI <megumi19901016@gmail.com>
2020-04-10 16:21:28 +0900

    リンクの変更

--- footer.php
+++ footer.php
@@ -30,9 +30,9 @@
     <section class="footer-nav">
       <h3 class="title-footer-nav">OFFICIAL SNS</h3>
       <ul class="footer-sns-nav">
-        <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/instagram-icon.svg" alt="インスタグラムへアクセス" width="10" height="10"></a></li>
-        <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/twitter-icon.svg" alt="ツイッターへアクセス" width="14" height="10"></a></li>
-        <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/facebook-icon.svg" alt="フェイスブックへアクセス" width="10" height="10"></a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/instagram-icon.svg" alt="インスタグラムへアクセス" width="10" height="10"></a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter-icon.svg" alt="ツイッターへアクセス" width="14" height="10"></a></li>
+        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook-icon.svg" alt="フェイスブックへアクセス" width="10" height="10"></a></li>
       </ul>
     </section>
   </div>
@@ -41,7 +41,7 @@
       <h3 class="title-footer-nav">ACCESS GUIDE</h3>
       <p>Feuille</p>
       <address>〒880-0811 宮崎県宮崎市錦町１丁目１<br><a class="tel" href="tel:0000-00-0000">TEL：0000-00-0000</a> / Mail：mag@example.com</address>
-      <nav class="btn-view-map"><a href="#">VIEW MAP<img src="<?php echo get_template_directory_uri() ?>/images/viewmap-icon.svg" alt="google mapを開く" width="10" height="10"></a></nav>
+      <nav class="btn-view-map"><a href="#">VIEW MAP<img src="<?php echo get_template_directory_uri(); ?>/images/viewmap-icon.svg" alt="google mapを開く" width="10" height="10"></a></nav>
     </div>
     <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3386.681869380904!2d131.42263511477398!3d31.915240034731255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3538b7c73abedf53%3A0x1e3b13cc6b1915a5!2z5qCq5byP5Lya56S-44G-44Gq44Gz44Go!5e0!3m2!1sja!2sjp!4v1583997519272!5m2!1sja!2sjp" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
   </section>
--- header.php
+++ header.php
@@ -1,15 +1,8 @@
-<html lang="ja">
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
   <head>
-    <meta charset="utf-8">
+    <meta charset="<?php echo esc_attr( bloginfo('charset'))?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Feuille</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/normalize.css">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-    <script src="<?php echo get_template_directory_uri() ?>/js/script.js"></script>
     <?php wp_head(); ?>
   </head>
 
@@ -17,14 +10,14 @@
     <div class="header-wrapper">
       <header class="header">
         <nav id="js_btn_menu" class="btn-menu">
-          <img class="btn-menu-close" src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">
-          <img class="btn-menu-open" src="<?php echo get_template_directory_uri() ?>/images/hamburger.svg" alt="ハンバーガーメニュー" width="20" height="14">
+          <img class="btn-menu-close" src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">
+          <img class="btn-menu-open" src="<?php echo get_template_directory_uri(); ?>/images/hamburger.svg" alt="ハンバーガーメニュー" width="20" height="14">
         </nav>
         <h1 class="logo"><a href="index.html"><img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" alt="Feuille" width="72" height="38"></a></h1>
         <nav class="pc-global-menu">
           <ul>
             <li><a href="#">NEW ITEM</a></li>
-            <li class="js-category-icon"><a href="category.html">CATEGORY<img src="<?php echo get_template_directory_uri() ?>/images/nav-downarrow.svg" alt="カテゴリーの詳細を表示" width="6" height="4"></a></li>
+            <li class="js-category-icon"><a href="category.html">CATEGORY<img src="<?php echo get_template_directory_uri(); ?>/images/nav-downarrow.svg" alt="カテゴリーの詳細を表示" width="6" height="4"></a></li>
             <li><a href="#">ABOUT US</a></li>
             <li><a href="#">GUIDE</a></li>
           </ul>
@@ -45,16 +38,16 @@
         <!-- ▼SP用nav▼ -->
         <nav class="header-nav">
           <ul>
-            <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/search-icon.svg" alt="検索する" width="21" height="20"></a></li>
-            <li class="sp-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/cart-icon.svg" alt="カートページへ" width="20" height="20"><span class="sp-cart-counter">0</span></a></li>
+            <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/search-icon.svg" alt="検索する" width="21" height="20"></a></li>
+            <li class="sp-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/cart-icon.svg" alt="カートページへ" width="20" height="20"><span class="sp-cart-counter">0</span></a></li>
           </ul>
         </nav>
         <div class="pc-header-nav">
           <nav class="pc-nav-block">
             <ul>
-              <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/login-icon.svg" alt="マイページへログインする" width="16" height="18">LOGIN</a></li>
-              <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/favorite-icon.svg" alt="お気に入り画面へアクセスする" width="20" height="18">FAVORITE</a></li>
-              <li class="pc-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/cart-icon.svg" alt="ショッピングカートへアクセスする" width="20" height="20"><span class="pc-cart-counter">0</span></a></li>
+              <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/login-icon.svg" alt="マイページへログインする" width="16" height="18">LOGIN</a></li>
+              <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/favorite-icon.svg" alt="お気に入り画面へアクセスする" width="20" height="18">FAVORITE</a></li>
+              <li class="pc-cart-icon"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/cart-icon.svg" alt="ショッピングカートへアクセスする" width="20" height="20"><span class="pc-cart-counter">0</span></a></li>
             </ul>
           </nav>
           <form id="search_form">
@@ -72,11 +65,11 @@
         <h2 class="hamburger-title-block">CHECKED ITEMS</h2>
         <p class="hamburger-sub-title">閲覧履歴</p>
         <ul class="checked-items">
-          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-01.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
-          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-02.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
-          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-03.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
-          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-04.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
-          <li><img src="<?php echo get_template_directory_uri() ?>/images/sp/checked-items-05.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/images/sp/checked-items-01.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri();?>/images/sp/checked-items-02.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/images/sp/checked-items-03.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/images/sp/checked-items-04.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/images/sp/checked-items-05.png" alt="ピアス閲覧商品" width="126" height="126"><P class="checked-items-price">¥00,000</p></li>
         </ul>
       </section>
       <div class="hamburger-main">
@@ -86,9 +79,9 @@
           <li id="js_hamburger_search_category" class="hamburger-search-category">
             <a>カテゴリーから探す</a>
             <ul id="js_humburger_category_child" class="hamburger-category-child">
-              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
-              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
-              <li><a href="category.html"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
+              <li><a href="category.html"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-arrow.svg" alt="子カテゴリーへアクセス" width="63" height="63">子カテゴリー名</a></li>
             </ul>
           </li>
         </ul>
@@ -106,11 +99,11 @@
           <li><a href="#">お問い合わせ</a></li>
         </ul>
         <ul class="hamburger-sns-block">
-          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-instagram.svg" alt="インスタグラムへアクセスする" width="57" height="57"></a></li>
-          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-twitter.svg" alt="ツイッターへアクセスする" width="71" height="57"></a></li>
-          <li><a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-facebook.svg" alt="フェイスブックへアクセスする" width="57" height="57"></a></li>
+          <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-instagram.svg" alt="インスタグラムへアクセスする" width="57" height="57"></a></li>
+          <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-twitter.svg" alt="ツイッターへアクセスする" width="71" height="57"></a></li>
+          <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-facebook.svg" alt="フェイスブックへアクセスする" width="57" height="57"></a></li>
         </ul>
-        <p class="js-btn-close"><img src="<?php echo get_template_directory_uri() ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">閉じる</p>
+        <p class="js-btn-close"><img src="<?php echo get_template_directory_uri(); ?>/images/sp/hamburger-closed.svg" alt="ハンバーガーメニューを閉じる" width="22" height="22">閉じる</p>
       </div>
     </div>
     <!-- ▲ハンバーガーメニュー終わり▲ -->
