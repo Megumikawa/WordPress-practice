30e4118ccf84d0b50e5e8eaa3a90a3a2ec1e4908
MEGUMI <megumi19901016@gmail.com>
2020-04-27 15:13:35 +0900

    スペースの調整

--- mytheme/index.php
+++ mytheme/index.php
@@ -16,7 +16,7 @@
 				</h1>
 			<?php endif; ?>
 
-			<?php if( have_posts() ): while( have_posts()): the_post(); ?>
+			<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
 			<article <?php post_class(); ?>>
 				<?php if( is_single() ): ?>
