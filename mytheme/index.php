dbccd59909af1bc5d35a6a73e27c20ac01f2ba7d
MEGUMI <megumi19901016@gmail.com>
2020-04-27 15:07:03 +0900

    スペース調整

--- mytheme/index.php
+++ mytheme/index.php
@@ -9,14 +9,14 @@
 				</h1>
 			<?php endif; ?>
 
-			<?php if(is_month()): ?>
+			<?php if( is_month() ): ?>
 				<h1 class="archive-title">
 					<i class="fa fa-clock-o"></i>
 					<?php echo get_the_date( 'Y年n月' ); ?>に投稿した記事
 				</h1>
 			<?php endif; ?>
 
-			<?php if( have_posts()): while( have_posts()): the_post(); ?>
+			<?php if( have_posts() ): while( have_posts()): the_post(); ?>
 
 			<article <?php post_class(); ?>>
 				<?php if( is_single() ): ?>
